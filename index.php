21dd786194664389ac11182b8ca20d97bd7a5d99
Cassia <cassiasa@yahoo.com.br>
2021-07-24 00:51:01 -0300

    PHP 7 - Curso Cod3r
    Seção 1 e 2 (até video 18)

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<!--MUDAR IDIOMA-->
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <!-- EXCLUÍDO PELO PROFESSOR:
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+  <!--FONTE DO GOOGLE FONTS-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+  <!--ESTILO CSS: clica com botão direito e seleciona "Copy Relative Path"-->
+  <link rel="stylesheet" href="assets\css\style.css">
+
+  <title>Curso PHP - Cod3r</title>
+</head>
+
+<body>
+  <header class="cabecalho">
+    <h1>Curso PHP</h1>
+    <h2>Índice dos Exercícios</h2>
+  </header>
+
+  <main class="principal">
+    <div class="conteudo">
+      <nav class="modulos">
+
+        <div class="modulo verde">
+          <h3>Módulo 01 - Básico</h3>
+          <ul>
+            <li><a href="#">Exercício a</a></li>
+            <li><a href="#">Exercício b</a></li>
+            <li><a href="#">Exercício c</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo vermelho">
+          <h3>Módulo 02 - </h3>
+          <ul>
+            <li><a href="#">Exercício a</a></li>
+            <li><a href="#">Exercício b</a></li>
+            <li><a href="#">Exercício c</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo azul">
+          <h3>Módulo 03 - </h3>
+          <ul>
+            <li><a href="#">Exercício a</a></li>
+            <li><a href="#">Exercício b</a></li>
+            <li><a href="#">Exercício c</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo roxo">
+          <h3>Módulo 04 - </h3>
+          <ul>
+            <li><a href="#">Exercício a</a></li>
+            <li><a href="#">Exercício b</a></li>
+            <li><a href="#">Exercício c</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo laranja">
+          <h3>Módulo 05 - </h3>
+          <ul>
+            <li><a href="#">Exercício a</a></li>
+            <li><a href="#">Exercício b</a></li>
+            <li><a href="#">Exercício c</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo verde-escuro">
+          <h3>Módulo 06 - </h3>
+          <ul>
+            <li><a href="#">Exercício a</a></li>
+            <li><a href="#">Exercício b</a></li>
+            <li><a href="#">Exercício c</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo vermelho-escuro">
+          <h3>Módulo 07 - </h3>
+          <ul>
+            <li><a href="#">Exercício a</a></li>
+            <li><a href="#">Exercício b</a></li>
+            <li><a href="#">Exercício c</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo azul-escuro">
+          <h3>Módulo 08 - </h3>
+          <ul>
+            <li><a href="#">Exercício a</a></li>
+            <li><a href="#">Exercício b</a></li>
+            <li><a href="#">Exercício c</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo roxo-escuro">
+          <h3>Módulo 09 - </h3>
+          <ul>
+            <li><a href="#">Exercício a</a></li>
+            <li><a href="#">Exercício b</a></li>
+            <li><a href="#">Exercício c</a></li>
+          </ul>
+        </div>
+
+      </nav>
+
+    </div>
+  </main>
+
+  <footer class="rodape">
+    <p>Curso PHP by Cod3r @ Udemy <?= date('Y'); ?></p>
+  </footer>
+
+</body>
+
+</html>
