8d3c3f4f43422bbb21f5c0b515e8d4a9cd354a3a
Cassia <cassiasa@yahoo.com.br>
2021-08-04 20:15:23 -0300

    conclusão módulo 3
    Várias coisas apagadas, por isso salvei a versão imediatamente anterior como um branch (Módulo 2 - integral)

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <!DOCTYPE html>
-<!--MUDAR IDIOMA-->
 <html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
-  <!-- EXCLUÍDO PELO PROFESSOR:
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-  <!--FONTE DO GOOGLE FONTS-->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
-  <!--ESTILO CSS: clica com botão direito e seleciona "Copy Relative Path"-->
   <link rel="stylesheet" href="assets\css\style.css">
 
   <title>Curso PHP - Cod3r</title>
@@ -29,88 +23,12 @@
 
         <div class="modulo verde">
           <h3>Módulo 01 - Básico</h3>
-          <ul>
-            <!-- <li><a href="exercicio.php">Exercício a</a></li> -->
+          <ul>            
             <li><a href="exercicio.php?dir=teste&file=teste">Exercício a</a></li>   
-            <li><a href="#">Exercício b</a></li>
-            <li><a href="#">Exercício c</a></li>
+            <!-- OBS: A pasta TESTE foi apagada -->
           </ul>
-        </div>
-
-        <div class="modulo vermelho">
-          <h3>Módulo 02 - </h3>
-          <ul>
-            <li><a href="#">Exercício a</a></li>
-            <li><a href="#">Exercício b</a></li>
-            <li><a href="#">Exercício c</a></li>
-          </ul>
-        </div>
-
-        <div class="modulo azul">
-          <h3>Módulo 03 - </h3>
-          <ul>
-            <li><a href="#">Exercício a</a></li>
-            <li><a href="#">Exercício b</a></li>
-            <li><a href="#">Exercício c</a></li>
-          </ul>
-        </div>
-
-        <div class="modulo roxo">
-          <h3>Módulo 04 - </h3>
-          <ul>
-            <li><a href="#">Exercício a</a></li>
-            <li><a href="#">Exercício b</a></li>
-            <li><a href="#">Exercício c</a></li>
-          </ul>
-        </div>
-
-        <div class="modulo laranja">
-          <h3>Módulo 05 - </h3>
-          <ul>
-            <li><a href="#">Exercício a</a></li>
-            <li><a href="#">Exercício b</a></li>
-            <li><a href="#">Exercício c</a></li>
-          </ul>
-        </div>
-
-        <div class="modulo verde-escuro">
-          <h3>Módulo 06 - </h3>
-          <ul>
-            <li><a href="#">Exercício a</a></li>
-            <li><a href="#">Exercício b</a></li>
-            <li><a href="#">Exercício c</a></li>
-          </ul>
-        </div>
-
-        <div class="modulo vermelho-escuro">
-          <h3>Módulo 07 - </h3>
-          <ul>
-            <li><a href="#">Exercício a</a></li>
-            <li><a href="#">Exercício b</a></li>
-            <li><a href="#">Exercício c</a></li>
-          </ul>
-        </div>
-
-        <div class="modulo azul-escuro">
-          <h3>Módulo 08 - </h3>
-          <ul>
-            <li><a href="#">Exercício a</a></li>
-            <li><a href="#">Exercício b</a></li>
-            <li><a href="#">Exercício c</a></li>
-          </ul>
-        </div>
-
-        <div class="modulo roxo-escuro">
-          <h3>Módulo 09 - </h3>
-          <ul>
-            <li><a href="#">Exercício a</a></li>
-            <li><a href="#">Exercício b</a></li>
-            <li><a href="#">Exercício c</a></li>
-          </ul>
-        </div>
-
+        </div> 
       </nav>
-
     </div>
   </main>
 
